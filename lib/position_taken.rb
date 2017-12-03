0f395877b13d9d8697846994887f0e722cb3cb67
Kavindu Amarasinghe <amarasinghekavindu@yahoo.com>
2017-12-03 06:42:23 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,10 @@
 # code your #position_taken? method here!
+
+def position_taken?(board, index)
+  converted = index.to_i
+  if converted === " " || ""
+    return false
+  else board[converted] === "X" || "O"
+    return true
+  end
+end
